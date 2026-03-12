237cde5e4eababd73afaf29f01eb29d977f04a6c
Strictlywoo <lpalma50771@ufide.ac.cr>
2026-03-11 20:52:37 -0600

    Proyecto Desarrollo Codigo 50%

---
+++ altoque/src/test/java/com/altoque/AltoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
